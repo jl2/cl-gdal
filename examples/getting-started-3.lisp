c8a54a8d6da286726c9e252b85b4510495e221a9
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2019-10-22 15:22:51 -0600

    More work on getting-started-3 vector example.
    Now displays metadata about layers and features.

--- examples/getting-started-3.lisp
+++ examples/getting-started-3.lisp
@@ -16,17 +16,47 @@
 
 (in-package :cl-gdal.examples)
 
-(defun getting-started-3 (&key
-                            (file-name 
-                             (autowrap:asdf-path '(:cl-gdal.examples "OSMP_Trails/OSMP_Trails.shp"))))
+(defun getting-started-3 (&key (file-name (autowrap:asdf-path '(:cl-gdal.examples "OSMP_Trails/OSMP_Trails.shp"))))
 
   (gdal:gdal-all-register)
 
   (let* ((np (cffi:null-pointer))
-         (dataset (gdal:gdal-open-ex file-name gdal:+of-vector+ np np np))
-         (layer (gdal:ogr-ds-get-layer dataset 0)))
-    (format t "Dataset~%Name: ~a~%Layer Count: ~a~%" (gdal:ogr-ds-get-name dataset) (gdal:ogr-ds-get-layer-count dataset))
-    (format t "~%Layer~%Name: ~a~%" (gdal:ogr-l-get-name layer))
-    
-    (autowrap:free layer)
+         (dataset       (gdal:gdal-open-ex file-name gdal:+of-vector+ np np np))
+         (layer-count   (gdal:ogr-ds-get-layer-count dataset)))
+    (format t "Dataset~%Name: ~a~%Layer Count: ~a~%" (gdal:ogr-ds-get-name dataset) layer-count)
+    (loop
+       for i below layer-count
+       do
+         (let ((layer (gdal:ogr-ds-get-layer dataset i)))
+           (format t "~%Layer ~a~%Name: ~a~%" i (gdal:ogr-l-get-name layer))
+           (gdal:ogr-l-reset-reading layer)
+
+           ;; TODO: Wrap this iteration in a macro or function
+           (loop
+              for feature = (gdal:ogr-l-get-next-feature layer)
+              until (cffi:null-pointer-p (gdalll::ogr-feature-h-ptr feature))
+              for fdefn = (gdal:ogr-l-get-layer-defn layer)
+              do
+                (format t "feature = ~a~%" feature)
+                (format t "fdefn = ~a name: ~a ~%field count: ~a~%"
+                        fdefn
+                        (gdal:ogr-fd-get-name fdefn)
+                        (gdal:ogr-fd-get-field-count fdefn))
+                (loop
+                   for i below (gdal:ogr-fd-get-field-count fdefn)
+                   for field-defn = (gdal:ogr-fd-get-field-defn fdefn i)
+                   for field-type = (gdal:ogr-fld-get-type field-defn)
+                   do
+                   ;; TODO: Make this a helper function
+                     (let ((value (cond ((= field-type gdal:+oft-integer+)
+                                         (gdal:ogr-f-get-field-as-integer feature i))
+                                        ((= field-type gdal:+oft-integer64+)
+                                         (gdal:ogr-f-get-field-as-integer64 feature i))
+                                        ((= field-type gdal:+oft-real+)
+                                         (gdal:ogr-f-get-field-as-double feature i))
+                                        (t
+                                         (gdal:ogr-f-get-field-as-string feature i)))))
+                       (format t "~a ~a (~a) = ~a~%" field-defn (gdal:ogr-fld-get-name-ref field-defn) i value)))
+                (autowrap:free feature))
+           (autowrap:free layer)))
     (autowrap:free dataset)))
